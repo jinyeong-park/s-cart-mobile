2b256dccd42f409eef436a1ea7623ca1845d5c4f
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-02-27 00:44:30 -0800

    basic styling with logo in the welcome page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground, Image } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
     <ImageBackground
       style={styles.background}
-      source={require('../assets/background.jpg')}
+      source={require('../assets/background3.png')}
     >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/sCart-logo.png')}
+        />
+        <Text>Find your taste</Text>
+
+      </View>
+
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -19,18 +28,29 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
+    opacity: 0.8
   },
   loginButton: {
     width: '100%',
     height: 70,
     backgroundColor: "#fc5c65"
-
   },
   registerButton: {
     width: '100%',
     height: 70,
-    backgroundColor: "#4ecdc4"
-
+    backgroundColor: "#383E56"
+  },
+  logoContainer: {
+    flexDirection: "row",
+    paddingRight: 100,
+    alignItems: "center",
+    bottom: 300,
+  },
+  logo: {
+    width: 140,
+    height: 100,
+    top: 0,
   }
 
 })
