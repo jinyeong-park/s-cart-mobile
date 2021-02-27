a99c9151540228521ae85b9043698b5209f7eb39
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-02-27 01:29:48 -0800

    encapsulate styles in the custom component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
+import AppText from './app/components/AppText';
 
 
 export default function App() {
