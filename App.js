86404e2c9cbed6b1a6370f6921848cca2ee41a6e
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-02-28 21:04:29 -0800

    add icons (close and trash)

--- App.js
+++ App.js
@@ -24,8 +24,8 @@ export default function App() {
     // </View>
 
     // <WelcomeScreen />
-    // <ViewImageScreen/>
-    <ListingDetailsScreen />
+    <ViewImageScreen/>
+    // <ListingDetailsScreen />
 
   )
 }
