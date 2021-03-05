44aaa5dbff652808b571d62dc53c6b1af2e18f3d
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-03-05 00:51:52 -0800

    refactoring

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -3,12 +3,13 @@ import { View, StyleSheet, TextInput } from 'react-native';
 import { MaterialCommunityIcons} from '@expo/vector-icons';
 
 import colors from '../config/colors';
+import defaultStyles from '../config/styles'
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
     <View style={styles.container}>
       {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/>}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
@@ -25,11 +26,6 @@ const styles = StyleSheet.create({
   icon: {
     margin: 10
   },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  }
 });
 
 export default AppTextInput;
