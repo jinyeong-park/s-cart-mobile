d312b245de365a8cdf4df24115fa064a2617ca5a
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-03-05 00:43:02 -0800

    reusable component of TextInput

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingScreens from './app/screens/ListingScreens';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
 
@@ -43,7 +44,10 @@ export default function App() {
     //   />
     // </Screen>
     // <AccountScreen/>
-    <ListingScreens />
+    // <ListingScreens />
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email"/>
+    </Screen>
 
 
 
