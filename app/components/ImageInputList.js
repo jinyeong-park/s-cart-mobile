48b96d45df862f6c0ce8762f969dd3fb1e334e57
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-03-10 21:58:08 -0800

    make Horizontal scroll to the end in camera section

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 import ImageInput from './ImageInput';
 
 // selecting multiple images
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map(uri => (
-        <View key={uri} style={styles.image}>
-          <ImageInput
-            imageUri={uri}
-            key={uri}
-            onChangeImage={() => onRemoveImage(uri)}
-          />
-        </View>
-      ))}
-      {/* adding new images */}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)}/>
-    </View>
+    <ScrollView
+      ref={scrollView}
+      horizontal={true}
+      onContentSizeChange={() => scrollView.current.scrollToEnd()}
+    >
+      <View style={styles.container}>
+        {imageUris.map(uri => (
+          <View key={uri} style={styles.image}>
+            <ImageInput
+              imageUri={uri}
+              key={uri}
+              onChangeImage={() => onRemoveImage(uri)}
+            />
+          </View>
+        ))}
+        {/* adding new images */}
+        <ImageInput onChangeImage={(uri) => onAddImage(uri)}/>
+      </View>
+    </ScrollView>
   );
 }
 
